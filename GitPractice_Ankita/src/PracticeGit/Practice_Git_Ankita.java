dcf7c4264bbf6caa00802dbe7c42b28d97f88cc9
smohapatra <smohapatra@172.20.10.6>
2023-10-14 09:41:33 +0530

    commit2

---
+++ GitPractice_Ankita/src/PracticeGit/Practice_Git_Ankita.java
@@ -0,0 +1,11 @@
+package PracticeGit;
+
+public class Practice_Git_Ankita {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("How are you doin'?");
+
+	}
+
+}
